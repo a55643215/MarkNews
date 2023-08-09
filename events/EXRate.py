8506c817c4189431802c8a61a0e62ea603053670
a55643215 <138569500+a55643215@users.noreply.github.com>
2023-08-09 16:33:21 +0800

    Add files via upload

--- events/EXRate.py
+++ events/EXRate.py
@@ -37,12 +37,12 @@ def getExchangeRate(msg):
     currency_list = msg[2:].split("/")
     currency = currency_list[0]
     currency1 = currency_list[1]
-    mony_value = currency_list[2]
+    money_value = currency_list[2]
     url_coinbase = 'https://api.coinbase.com/v2/exchange-rates?currency=' + currency   
     res = requests.get(url_coinbase)
     jData = res.json()
     pd_currency = jData['data']['rates']
     content = f'目前的兌換率為:{pd_currency[currency1]}{currency1}  \n查詢的金額為: '
     amount= float(pd_currency[currency1])
-    content+=str('%.2f' % (amount * float(mony_value))) + " " + currency1
+    content+=str('%.2f' % (amount * float(money_value))) + " " + currency1
     return content
